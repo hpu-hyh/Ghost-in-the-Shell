1e659c5ae92e51c95f88e6eb69eb32e3e210d16e
hpu-hyh <2787281908@qq.com>
2022-06-11 20:53:53 +0800

    vvv

---
+++ VUE学习/vue_test/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <img src="./assets/logo.png" alt="">
+        <schoolName></schoolName>
+    </div>
+</template>
+
+<script>
+//引入组件
+import schoolName from './components/SchoolName'
+export default {
+    name: 'App',
+    components:{
+        schoolName,
+    }
+}
+</script>
+
+<style>
+</style>
